af0a1297a13d16aa3c26f3e76c0466c43e0aa294
DaryaSergeevna <intencia96@gmail.com>
2019-10-26 02:25:40 +0300

    добавлена история операций

---
+++ History.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Calculator
+{
+    class History
+    {
+        public decimal firstNum;
+        public string operation;
+        public decimal secondNum;
+        public decimal answer;
+        public History()
+        {
+
+        }
+    }
+}
